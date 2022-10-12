1ef176e1d81deb2485ce60aac4a91445add9ce12
Смирнов Вячеслав <slava_smirnov@mail.ru>
2022-10-13 00:01:15 +0300

    feat: add comments

--- bot.py
+++ bot.py
@@ -6,14 +6,18 @@ import config
 
 bot = telebot.TeleBot(config.token)
 
-
+'''
+функция которая возвращает кнопку в телеграмм-канале
+'''
 def get_markup():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn = types.KeyboardButton("❤Получить приятность")
     markup.add(btn)
     return markup
 
-
+'''
+стартовое сообщение
+'''
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,
@@ -23,7 +27,9 @@ def start(message):
                      )
     # bot.send_message(message.from_user.id, compliments.text_message())
 
-
+'''
+когда бот уже активно принимает сообщения
+'''
 @bot.message_handler(content_types=['text'])
 def send_message(message):
     if message.text == "❤Получить приятность":
